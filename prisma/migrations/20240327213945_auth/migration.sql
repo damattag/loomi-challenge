df1fc532052c85a3375b75c1ce5b0f4bd69f01a6
damattag <guilherme.damatta@yahoo.com.br>
2024-03-27 18:40:30 -0300

    migrations and example of env

---
+++ prisma/migrations/20240327213945_auth/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "email_token_expiry" TIMESTAMP(3),
+ADD COLUMN     "email_verification_token" TEXT,
+ADD COLUMN     "email_verified" BOOLEAN NOT NULL DEFAULT false;
