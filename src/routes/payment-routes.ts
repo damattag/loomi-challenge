c1c40fe976d0337887394a9b47462d124c212aad
damattag <guilherme.damatta@yahoo.com.br>
2024-04-02 16:26:37 -0300

    feat(feat/sales-report): authenticate all routes

--- src/routes/payment-routes.ts
+++ src/routes/payment-routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 
 import PaymentController from '@http/controllers/payment-controller';
+import { verifyJwt } from '@http/middlewares/verify-jwt';
 
 const paymentRouter = Router();
+
+paymentRouter.use(verifyJwt);
 
 paymentRouter.route('/:orderId').patch(PaymentController.pay);
 
--- src/routes/sales-report-routes.ts
+++ src/routes/sales-report-routes.ts
@@ -2,7 +2,11 @@ import { Router } from 'express';
 
 import SalesReportController from '@http/controllers/sales-report-controller';
 
+import { verifyPermission } from '@http/middlewares/verify-permission';
+
 const salesReportRouter = Router();
+
+salesReportRouter.use(verifyPermission);
 
 salesReportRouter
   .route('/')
